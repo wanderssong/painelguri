5f57b70f800c2a0a0786946cab68f0778d52a13f
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-08-12 17:37:07 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-const ultimaAtualizacao = "07/08/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "12/08/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "MERCADO GURI",
@@ -9,11 +9,11 @@ const dadosEmpresas = [
         ],
         gastos: [
             { data: "07-08-2025", valor: 90.00, descricao: "Campanhas dos Pais" },
+            { data: "13-08-2025", valor: 90.00, descricao: "Ofertas da semana" },
             
 
         ]
     },
     
     
-
 ];
